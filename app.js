c62a262bf9405ecd9069c873e8fd40d9f98037f1
kurtfriedrichhotmail <kurtfriedrich@hotmail.com>
2019-10-13 16:49:45 -0700

    first push

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const taskController = require("./controllers/TaskController");
+const cors = require('cors')  // using this module to solve CORS problem
+// note the extra line in package.json to download this code
+
+var corsOptions = {
+  origin: 'http://localhost:4200',   // this URL must match the URL that the Angular app will call from
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}
+
+// this brings in and sets up the monog db instance connection
+require("./config/db");
+
+const app = express();
+
+const port = process.env.PORT || 3000;  // setting the port number for this server
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+app.use(cors(corsOptions))   // bringing in the CORS code to our app
+
+// API ENDPOINTS
+// not using the Express Router code, instead just listing them
+// each of these 5 routed call one of the 5 methods defined in taskController
+// which in turn call Mongo Atlas, each of those 5 do a return to the client
+// notive they are "keyed", but HTTP request type, get, put, etc
+app
+  .route("/tasks")
+  .get(taskController.listAllTasks)
+  .post(taskController.createNewTask);
+
+app
+  .route("/tasks/:taskid")
+  .get(taskController.readTask)
+  .put(taskController.updateTask)
+  .delete(taskController.deleteTask);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
---
+++ controllers/TaskController.js
@@ -0,0 +1,59 @@
+const Task = require("../models/Task");
+
+exports.listAllTasks = (req, res) => {
+  Task.find({}, (err, task) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+    res.status(200).json(task);
+    console.log(task);
+  });
+};
+
+exports.createNewTask = (req, res) => {
+  let newTask = new Task(req.body);
+  console.log(newTask);
+  newTask.save((err, task) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(201).json(task);
+  });
+};
+
+exports.readTask = (req, body) => {
+  Task.findById(req.params.taskid, (err, task) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).json(task);
+  });
+};
+
+exports.updateTask = (req, res) => {
+  console.log('task id at server is ' + req.params.taskid);
+  Task.findOneAndUpdate(
+    { _id: req.params.taskid },  // don't know who changed the name from _id
+    req.body,
+    { new: true },  // true or false to let it add if not present?
+    (err, task) => {
+      if (err) {
+        res.status(500).send(err);
+      }
+      console.log(task);
+      res.status(200).json(task);
+    }
+  );
+};
+
+
+
+exports.deleteTask = (req, res) => {
+  Task.remove({ _id: req.params.taskid }, (err, task) => {  // don't know who changed the name from _id
+    if (err) {
+      res.status(404).send(err);
+    }
+    res.status(200).json({ message: "Task successfully deleted" });
+  });
+};
